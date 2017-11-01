ed2d97ee4567ceaf5144fe11c850f3c64a3b956a
Aurélie Digeon <aurelie.digeon@gmail.com>
2017-11-01 14:31:00 +0100

    Add news assets

--- Assets/Scripts/Tests/Interactions/InteractionTest.cs
+++ Assets/Scripts/Tests/Interactions/InteractionTest.cs
@@ -6,10 +6,15 @@ using UnityEngine.Events;
 //Classe de test d'interactions associée à un objet proposant d'être observé et utilisé
 public class InteractionTest : MonoBehaviour, IInteractable {
 	private InteractionManager im;
+	private Dictionary<InteractionType, UnityAction> availableInteractions;
 
 	// Use this for initialization
 	void Start () {
 		im = InteractionManager.Instance;	
+
+		// Add available interactions
+		availableInteractions.Add(InteractionType.Observe, new UnityAction (Observe));
+		availableInteractions.Add(InteractionType.Use, new UnityAction (Use));
 	}
 	
 	// Update is called once per frame
@@ -19,10 +24,12 @@ public class InteractionTest : MonoBehaviour, IInteractable {
 
 	public Dictionary<InteractionType, UnityAction> GetInteractions (GameObject source) {
 		Dictionary<InteractionType, UnityAction> interactions = new Dictionary<InteractionType, UnityAction> ();
-		if (im.CanInteract(source, gameObject, InteractionType.Observe))
-			interactions.Add (InteractionType.Observe, new UnityAction (Observe));
-		if (im.CanInteract (source, gameObject, InteractionType.Use))
-			interactions.Add (InteractionType.Use, new UnityAction (Use));
+
+		foreach (KeyValuePair<InteractionType, UnityAction> pair in availableInteractions) {
+			if (im.CanInteract(source, gameObject, pair.Key))
+				interactions.Add (pair.Key, new UnityAction (pair.Value));
+		}
+
 		return interactions;
 	}
 
