32311434be64d6bc0652c1f4fb82a309af4f2086
Aurélie Digeon <aurelie.digeon@gmail.com>
2017-11-13 00:20:17 +0100

    Add trapdoor

---
+++ Assets/Scripts/Events/EventCausality.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventCausality : MonoBehaviour {
+	void Awake () {
+		EventManager.AddCausality ("UseTrapdoorSwitch", "OpenTrapdoor");
+		Debug.Log ("Added UseTrapdoorSwitch -> OpenTrapdoor causality");
+	}
+}
---
+++ Assets/Scripts/TrapDoorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrapDoorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		EventManager.StartListening ("OpenTrapdoor", new UnityAction (open));
+	}
+
+	public void open() {
+		this.GetComponent<Animation>().Play("TrapAnimation");
+	}
+}
