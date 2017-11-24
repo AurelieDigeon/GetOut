5d163646489e702e6650b6c6ad78de4176ad0ca0
Chostakovitch <quentinduchemin@gmail.com>
2017-11-24 01:26:02 +0100

    Added TV meuble case Use and animation

---
+++ Assets/Scripts/ObjectController/TVCaseController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TVCaseController : InteractionBase {
+	private Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+
+		availableInteractions.Add (InteractionType.Use, new UnityAction (ToggleCase));
+	}
+	
+	private void ToggleCase() {
+		animator.SetBool ("isOpen", !animator.GetBool ("isOpen"));
+	}
+}
