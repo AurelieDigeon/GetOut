8d5a32d809fcbf5d6b51fa75c5fc4528d70ea3b5
Aurélie Digeon <aurelie.digeon@gmail.com>
2017-12-11 20:47:04 +0100

    Add VR

---
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+
+	public float speed;
+	public GameObject cam;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	void Update()
+	{
+
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 tempDir = cam.transform.forward;
+		tempDir.y = 0;
+
+		Vector3 htempDir = cam.transform.right;
+		htempDir.y = 0;
+
+
+		//rb.MovePosition (transform.position + cam.transform.forward + new Vector3(h,0,v)*Time.deltaTime*speed); 
+
+		if (v < 0)
+		{
+			rb.MovePosition (transform.position - tempDir*speed + new Vector3(0,0,v)*Time.deltaTime*speed); 
+		}
+		else if (v > 0)
+		{
+			rb.MovePosition (transform.position + tempDir*speed + new Vector3(0,0,v)*Time.deltaTime*speed); 
+		}
+
+		if (h < 0)
+		{
+			rb.MovePosition (transform.position - htempDir*speed + new Vector3(h,0,0)*Time.deltaTime*speed); 
+		}
+		else if (h> 0)
+		{
+			rb.MovePosition (transform.position + htempDir*speed + new Vector3(h,0,0)*Time.deltaTime*speed); 
+		}
+	}
+}
---
+++ Assets/Scripts/Player/RightHandController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.Events;
+
+public class RightHandController : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		Vector3 handPosition = InputTracking.GetLocalPosition(VRNode.RightHand);
+		Vector3 headPosition = InputTracking.GetLocalPosition (VRNode.CenterEye);
+		transform.position = Camera.main.transform.position - (headPosition - handPosition);
+		Vector3 rotAngles = InputTracking.GetLocalRotation (VRNode.RightHand).eulerAngles + new Vector3 (90, 0, 0);
+		transform.rotation = Quaternion.Euler(rotAngles);
+	}
+
+	void OnCollisionEnter(Collision col) {
+		GameObject other = col.gameObject;
+		InteractionBase inter;
+		if ((inter = other.GetComponent<InteractionBase> ()) != null) {
+			UnityAction use;
+			if (inter.GetInteractions(gameObject).TryGetValue(InteractionType.Use, out use)) {
+				use.Invoke ();
+			}
+		}
+	}
+}
